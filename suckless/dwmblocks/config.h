8cada286a1ee23821bd4191b829ab47a60cefeb4
cyfraka <cyfraka@gmail.com>
2020-05-05 17:01:55 +0200

    update

--- suckless/dwmblocks/config.h
+++ suckless/dwmblocks/config.h
@@ -1,19 +1,24 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	/*{"cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
-	{"pacpackages",	0,	8},
-	/* {"cpu",		10,	13}, */
-	/* {"memory",	10,	14}, */
-	/* {"moonphase",	18000,	5}, */
-	/* {"mailbox",	180,	12}, */
-	{"weather",	600,	5},
-	/* {"news",	0,	6}, */
-	{"internet",	5,	4},
-	{"light",	0,	10},
-	{"volume",	0,	10},
-	{"battery",	5,	3},
-	{"clock",	60,	1},
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	/* {"",	"music",	0,	11}, */
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"news",		0,	6}, */
+	/* {"",	"crypto",	0,	13}, */
+	/* {"",	"torrent",	20,	7}, */
+	/* {"",	"memory",	10,	14}, */
+	/* {"",	"cpu",		10,	13}, */
+	/* {"",	"moonphase",	18000,	5}, */
+	{"",	"weather",	18000,	5},
+	/* {"",	"mailbox",	180,	12}, */
+	/* {"",	"nettraf",	1,	16}, */
+	{"",	"internet",	5,	4},
+	{"",	"volume",	0,	10},
+	{"",	"light",	0,	10},
+	{"",	"battery | tr \'\n\' \' \'",	5,	3},
+	{"",	"clock",	60,	1},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
@@ -22,4 +27,4 @@ static char delim = '|';
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
-// autocmd BufWritePost */dwmblocks/config.h !sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
--- suckless/dwmblocks/dwmblocks.c
+++ suckless/dwmblocks/dwmblocks.c
@@ -5,9 +5,10 @@
 #include<signal.h>
 #include<X11/Xlib.h>
 #define LENGTH(X)               (sizeof(X) / sizeof (X[0]))
-#define CMDLENGTH		50
+#define CMDLENGTH		70
 
 typedef struct {
+	char* icon;
 	char* command;
 	unsigned int interval;
 	unsigned int signal;
@@ -15,6 +16,7 @@ typedef struct {
 void sighandler(int num);
 void buttonhandler(int sig, siginfo_t *si, void *ucontext);
 void replace(char *str, char old, char new);
+void remove_all(char *str, char to_remove);
 void getcmds(int time);
 #ifndef __OpenBSD__
 void getsigcmds(int signal);
@@ -34,8 +36,6 @@ static int screen;
 static Window root;
 static char statusbar[LENGTH(blocks)][CMDLENGTH] = {0};
 static char statusstr[2][256];
-static char exportstring[CMDLENGTH + 22] = "export BLOCK_BUTTON=-;";
-static int button = 0;
 static int statusContinue = 1;
 static void (*writestatus) () = setroot;
 
@@ -47,6 +47,19 @@ void replace(char *str, char old, char new)
 			str[i] = new;
 }
 
+void remove_all(char *str, char to_remove) {
+	char *read = str;
+	char *write = str;
+	while (*read) {
+		if (*read == to_remove) {
+			read++;
+			*write = *read;
+		}
+		read++;
+		write++;
+	}
+}
+
 //opens process *cmd and stores output in *output
 void getcmd(const Block *block, char *output)
 {
@@ -55,25 +68,16 @@ void getcmd(const Block *block, char *output)
 		output[0] = block->signal;
 		output++;
 	}
-	char* cmd;
-	FILE *cmdf;
-	if (button)
-	{
-		cmd = strcat(exportstring, block->command);
-		cmd[20] = '0' + button;
-		button = 0;
-		cmdf = popen(cmd,"r");
-		cmd[22] = '\0';
-	}
-	else
-	{
-		cmd = block->command;
-		cmdf = popen(cmd,"r");
-	}
+	strcpy(output, block->icon);
+	char *cmd = block->command;
+	FILE *cmdf = popen(cmd,"r");
 	if (!cmdf)
 		return;
-	fgets(output, CMDLENGTH, cmdf);
-	int i = strlen(output);
+	char c;
+	int i = strlen(block->icon);
+	fgets(output+i, CMDLENGTH-i, cmdf);
+	remove_all(output, '\n');
+	i = strlen(output);
 	if (delim != '\0' && i)
 		output[i++] = delim;
 	output[i++] = '\0';
@@ -117,6 +121,7 @@ void setupsignals()
 	sa.sa_sigaction = buttonhandler;
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
+	signal(SIGCHLD, SIG_IGN);
 
 }
 #endif
@@ -179,9 +184,29 @@ void sighandler(int signum)
 
 void buttonhandler(int sig, siginfo_t *si, void *ucontext)
 {
-	button = si->si_value.sival_int & 0xff;
-	getsigcmds(si->si_value.sival_int >> 8);
+	int button = si->si_value.sival_int & 0xff;
+	sig = si->si_value.sival_int >> 8;
+	getsigcmds(sig);
 	writestatus();
+	if (fork() == 0)
+	{
+		static char exportstring[CMDLENGTH + 22] = "export BLOCK_BUTTON=-;";
+		const Block *current;
+		int i;
+		for (i = 0; i < LENGTH(blocks); i++)
+		{
+			current = blocks + i;
+			if (current->signal == sig)
+				break;
+		}
+		char *cmd = strcat(exportstring, blocks[i].command);
+		cmd[20] = '0' + button;
+		char *command[] = { "/bin/sh", "-c", cmd, NULL };
+		setsid();
+		execvp(command[0], command);
+		exit(EXIT_SUCCESS);
+		cmd[22] = '\0';
+	}
 }
 
 #endif
